77f8247242f94a72a1395098bf43374954706afb
superpig <940578704@qq.com>
2020-06-24 23:42:02 +0800

    feat: finish part10 exercise

---
+++ src/part10/spi/interpreter.ts
@@ -0,0 +1,117 @@
+import Parser, { Num, BinOp, AST, UaryOp, NoOp, Compound, Assign, Var, Program, Block, VarDecl, Type } from './parser'
+import { TokenType } from './token'
+const { PLUS, MINUS, MUL, INTEGER_DIV, FLOAT_DIV } = TokenType
+
+export abstract class NodeVisitor {
+  abstract visit(node: AST)
+}
+
+/*
+ * 简单的 Pascal 解释器
+ */
+export default class Interpreter extends NodeVisitor {
+  // 模拟符号表，存储变量的值
+  public GLOBAL_SCOPE: {
+    [key: string]: number
+  } = {}
+  private parser: Parser
+  constructor(parser: Parser) {
+    super()
+    this.parser = parser
+    this.GLOBAL_SCOPE = {}
+  }
+  public interpret() {
+    const tree = this.parser.parse()
+    if (!tree) return ''
+    return this.visit(tree)
+  }
+  public visitCompound(node: Compound) {
+    for (const child of node.children) {
+      this.visit(child)
+    }
+  }
+  public visit(node: AST) {
+    if (node instanceof BinOp) {
+      return this.visitBinOp(node)
+    } else if (node instanceof Num) {
+      return this.visitNum(node)
+    } else if (node instanceof UaryOp) {
+      return this.visitUnaryOp(node)
+    } else if (node instanceof Compound) {
+      return this.visitCompound(node)
+    } else if (node instanceof Assign) {
+      return this.visitAssign(node)
+    } else if (node instanceof NoOp) {
+      return this.visitNoOp(node)
+    } else if (node instanceof Var) {
+      return this.visitVar(node)
+    } else if (node instanceof Program) {
+      return this.visitProgram(node)
+    } else if (node instanceof Block) {
+      return this.visitBlock(node)
+    } else if (node instanceof VarDecl) {
+      return this.visitVarDecl(node)
+    } else if (node instanceof Type) {
+      return this.visitType(node)
+    } else {
+      const name = node && node.constructor.name
+      throw new Error(`No visit${name} method`)
+    }
+  }
+  private visitProgram(node: Program) {
+    this.visit(node.block)
+  }
+  private visitBlock(node: Block) {
+    for (const declaration of node.declarations) {
+      this.visit(declaration)
+    }
+    this.visit(node.compoundStatement)
+  }
+  private visitBinOp(node: BinOp) {
+    const op = node.op.type
+    if (op === PLUS) {
+      return this.visit(node.left) + this.visit(node.right)
+    } else if (op === MINUS) {
+      return this.visit(node.left) - this.visit(node.right)
+    } else if (op === MUL) {
+      return this.visit(node.left) * this.visit(node.right)
+    } else if (op === INTEGER_DIV) {
+      return Math.floor(this.visit(node.left) / this.visit(node.right))
+    } else if (op === FLOAT_DIV) {
+      return this.visit(node.left) / this.visit(node.right)
+    }
+  }
+  private visitUnaryOp(node: UaryOp) {
+    const op = node.op.type
+    if (op === PLUS) {
+      return +this.visit(node.expr)
+    } else if (op === MINUS) {
+      return -this.visit(node.expr)
+    }
+  }
+  private visitAssign(node: Assign) {
+    const varName = node.left.value
+    this.GLOBAL_SCOPE[varName] = this.visit(node.right)
+  }
+  private visitVar(node: Var) {
+    const varName = node.value
+    const val = this.GLOBAL_SCOPE[varName]
+    if (typeof val === 'undefined') {
+      throw new Error(`variable ${varName} is undefined`)
+    } else {
+      return val
+    }
+  }
+  private visitNoOp(node: NoOp) {
+    // do nothing
+  }
+  private visitNum(node: Num) {
+    return node.value
+  }
+  private visitVarDecl(node) {
+    // do nothing
+  }
+  private visitType(node) {
+    // do nothing
+  }
+}
---
+++ src/part10/spi/lexer.ts
@@ -0,0 +1,213 @@
+import { TokenType, Token } from './token'
+
+const {
+  BEGIN,
+  END,
+  ID,
+  ASSIGN,
+  SEMI,
+  DOT,
+  PROGRAM,
+  VAR,
+  INTEGER_DIV,
+  FLOAT_DIV,
+  INTEGER,
+  REAL,
+  REAL_CONST,
+  INTEGER_CONST,
+  MINUS,
+  PLUS,
+  MUL,
+  LPAREN,
+  RPAREN,
+  COLON,
+  COMMA,
+  EOF
+} = TokenType
+
+const RESERVED_KEYWORDS = {
+  PROGRAM: new Token(PROGRAM, 'PROGRAM'),
+  VAR: new Token(VAR, 'VAR'),
+  DIV: new Token(INTEGER_DIV, 'DIV'),
+  INTEGER: new Token(INTEGER, 'INTEGER'),
+  REAL: new Token(REAL, 'REAL'),
+  BEGIN: new Token(BEGIN, 'BEGIN'),
+  END: new Token(END, 'END')
+}
+/*
+ * 词法分析器
+ */
+export default class Lexer {
+  private text: string = ''
+  private pos: number = 0
+  private currentChar: string = ''
+  constructor(text: string) {
+    this.text = text
+    this.pos = 0
+    this.currentChar = this.text.charAt(this.pos)
+  }
+  public getNextToken(): Token {
+    while (this.currentChar !== '') {
+      if (this.isWhitespace(this.currentChar)) {
+        this.skipWhitespace()
+        continue
+      }
+
+      if (this.currentChar === '{') {
+        this.advance()
+        this.skipComment()
+        continue
+      }
+
+      if (this.isAlpha(this.currentChar)) {
+        return this.id()
+      }
+
+      if (this.isDigit(this.currentChar)) {
+        return this.number()
+      }
+
+      if (this.currentChar === ':' && this.peek() === '=') {
+        this.advance()
+        this.advance()
+        return new Token(ASSIGN, ':=')
+      }
+
+      if (this.currentChar === ';') {
+        this.advance()
+        return new Token(SEMI, ';')
+      }
+
+      if (this.currentChar === ':') {
+        this.advance()
+        return new Token(COLON, this.currentChar)
+      }
+
+      if (this.currentChar === ',') {
+        this.advance()
+        return new Token(COMMA, this.currentChar)
+      }
+
+      if (this.currentChar === '-') {
+        const token = new Token(MINUS, this.currentChar)
+        this.advance()
+        return token
+      }
+
+      if (this.currentChar === '+') {
+        const token = new Token(PLUS, this.currentChar)
+        this.advance()
+        return token
+      }
+
+      if (this.currentChar === '*') {
+        const token = new Token(MUL, this.currentChar)
+        this.advance()
+        return token
+      }
+
+      if (this.currentChar === '/') {
+        const token = new Token(FLOAT_DIV, this.currentChar)
+        this.advance()
+        return token
+      }
+
+      if (this.currentChar === '(') {
+        const token = new Token(LPAREN, this.currentChar)
+        this.advance()
+        return token
+      }
+
+      if (this.currentChar === ')') {
+        const token = new Token(RPAREN, this.currentChar)
+        this.advance()
+        return token
+      }
+
+      if (this.currentChar === '.') {
+        this.advance()
+        return new Token(DOT, '.')
+      }
+
+      this.error()
+    }
+    return new Token(EOF, null)
+  }
+  /**
+   * 从输入中读取整数或浮点数
+   */
+  private number() {
+    let result = ''
+    let token = null
+    while (this.currentChar !== '' && this.isDigit(this.currentChar)) {
+      result += this.currentChar
+      this.advance()
+    }
+    if (this.currentChar === '.') {
+      result += this.currentChar
+      this.advance()
+      // @ts-ignore
+      while (this.currentChar !== '' && this.isDigit(this.currentChar)) {
+        result += this.currentChar
+        this.advance()
+      }
+      token = new Token(REAL_CONST, parseFloat(result))
+    } else {
+      token = new Token(INTEGER_CONST, parseInt(result))
+    }
+    return token
+  }
+  private advance(): void {
+    this.pos++
+    if (this.pos > this.text.length - 1) {
+      this.currentChar = ''
+    } else {
+      this.currentChar = this.text.charAt(this.pos)
+    }
+  }
+  private skipWhitespace(): void {
+    while (this.currentChar && this.isWhitespace(this.currentChar)) {
+      this.advance()
+    }
+  }
+  private skipComment(): void {
+    while (this.currentChar !== '}') {
+      this.advance()
+    }
+    this.advance()
+  }
+  private isWhitespace(char: string) {
+    return /\s/.test(char)
+  }
+  private isDigit(char: string): boolean {
+    return char && !isNaN(+char)
+  }
+  /**
+   * 处理标识符和关键字
+   */
+  private id() {
+    let result = ''
+    while (this.currentChar !== '' && this.isalnum(this.currentChar)) {
+      result += this.currentChar
+      this.advance()
+    }
+    const token = RESERVED_KEYWORDS[result] || new Token(ID, result)
+    return token
+  }
+  private isalnum(str: string): boolean {
+    return /^[0-9a-zA-Z]*$/.test(str)
+  }
+  private isAlpha(char: string): boolean {
+    return /^[A-Z]$/i.test(char)
+  }
+  private peek() {
+    const peekPos = this.pos + 1
+    if (peekPos > this.text.length - 1) {
+      return null
+    }
+    return this.text[peekPos]
+  }
+  private error(): void {
+    throw new Error(`Invalid character`)
+  }
+}
